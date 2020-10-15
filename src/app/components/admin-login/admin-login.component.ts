3a8c38b4c07ac33bd050ff5f392bab873cacf7f4
ch_soumya <chsowmya505@gmail.com>
2020-10-14 21:15:53 -0500

    admin login css

--- src/app/components/admin-login/admin-login.component.ts
+++ src/app/components/admin-login/admin-login.component.ts
@@ -8,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class AdminLoginComponent implements OnInit {
 
   constructor() {
-    console.log("location: ", window.location.href);
    }
 
   ngOnInit(): void {
+    if(!window.location.href.toString().endsWith("index.html")) {
+      let id = document.getElementById("headermain") as HTMLElement;
+      console.log("tag: ", id);
+      if(id.classList.length == 0) {   
+        id.classList.add("background-header");
+      }
+    }
   }
 
 }
